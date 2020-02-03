3694e36dba4acf1279aa9217a2b75454e7f0cb2e
Takafumi Suzuki <highdro0479@gmail.com>
2020-02-03 15:02:09 +0900

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,15 @@
-import * as React from 'react'
-import Link from 'next/link'
-import Layout from '../components/Layout'
-import { NextPage } from 'next'
+import * as React from "react";
+import Layout from "../components/Layout";
+import { NextPage } from "next";
+import Canvas from "../utils/canvas";
 
 const IndexPage: NextPage = () => {
-  return (
-    <Layout title="Home | Next.js + TypeScript Example">
-      <h1>Hello Next.js 👋</h1>
-      <p>
-        <Link href="/about">
-          <a>About</a>
-        </Link>
-      </p>
-    </Layout>
-  )
-}
+    return (
+        <Layout title="Home | Next.js + TypeScript Example">
+            <h1>Hello Next.js 👋</h1>
+            <Canvas></Canvas>
+        </Layout>
+    );
+};
 
-export default IndexPage
+export default IndexPage;
---
+++ utils/canvas.tsx
@@ -0,0 +1,85 @@
+import React, { useEffect, useRef } from "react";
+
+const Canvas: React.FC = () => {
+    const canvasRef = useRef<HTMLCanvasElement>(null);
+    const mouseCoord = useRef({ x: 0, y: 0 });
+
+    const getContext = (): CanvasRenderingContext2D => {
+        const canvas: HTMLCanvasElement = canvasRef.current!;
+        return canvas.getContext("2d")!;
+    };
+
+    const initialize = () => {
+        const canvas: any = canvasRef.current;
+        canvas.width = 640;
+        canvas.height = 480;
+    };
+
+    const drawQuadraticBezier = (
+        context: CanvasRenderingContext2D,
+        x1: number,
+        y1: number,
+        x2: number,
+        y2: number,
+        cx: number,
+        cy: number,
+        color?: string,
+        width: number = 1
+    ) => {
+        if (color != null) {
+            context.strokeStyle = color;
+        }
+
+        context.lineWidth = width;
+        context.beginPath();
+        context.moveTo(x1, y1);
+        context.quadraticCurveTo(cx, cy, x2, y2);
+        context.closePath();
+        context.stroke();
+    };
+
+    const canvasRender = (context: CanvasRenderingContext2D) => {
+        context.fillStyle = "#ffffff";
+        context.fillRect(
+            0,
+            0,
+            canvasRef.current!.width,
+            canvasRef.current!.height
+        );
+        drawQuadraticBezier(
+            context,
+            100,
+            100,
+            100,
+            300,
+            mouseCoord.current.x,
+            mouseCoord.current.y,
+            "#ff9900"
+        );
+    };
+
+    useEffect(() => {
+        initialize();
+        const context: CanvasRenderingContext2D = getContext();
+        const onMouseMoved = (e: MouseEvent) => {
+            mouseCoord.current.x = e.clientX;
+            mouseCoord.current.y = e.clientY;
+            canvasRender(context);
+        };
+        window.addEventListener("mousemove", e => onMouseMoved(e));
+
+        canvasRender(context);
+
+        return () => {
+            window.removeEventListener("mousemove", onMouseMoved);
+        };
+    }, []);
+
+    return (
+        <>
+            <canvas className="main_canvas" ref={canvasRef}></canvas>
+        </>
+    );
+};
+
+export default Canvas;
