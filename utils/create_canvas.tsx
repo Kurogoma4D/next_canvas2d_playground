809108fa512a3b93ebf48d822011d077994aeb83
Takafumi Suzuki <highdro0479@gmail.com>
2020-02-15 18:36:55 +0900

    modify: functionize splash

--- utils/create_canvas.tsx
+++ utils/create_canvas.tsx
@@ -1,7 +1,7 @@
 /// <reference types="../node_modules/@types/createjs/" />
 
 import React, { useEffect, useRef } from "react";
-import { buildTextline } from "./splash_animation";
+import { buildLoading } from "./splash_animation";
 import SimplexNoise from "simplex-noise";
 
 export type Coord = {
@@ -28,49 +28,15 @@ const CreateCanvas: React.FC = () => {
     s = new createjs.Stage(canvas);
 
     const background = new createjs.Shape();
-    const text = new createjs.Container();
+    const text = buildLoading(canvas);
 
     background.graphics
       .beginFill("#F5D4D3")
       .drawRect(0, 0, canvas.width, canvas.height);
 
-    text.scaleX = text.scaleY = 0.6;
-    text.regX = canvas.width / 2;
-    text.regY = canvas.height / 2;
-    text.y = canvas.height / 2;
-
-    const first = buildTextline(
-      canvas.width,
-      "クリエイターなら、",
-      canvas.height / 2.2,
-      500,
-      "#F78E8A"
-    );
-
-    const second = buildTextline(
-      canvas.width,
-      "つくり続けた奴が正義だ。",
-      canvas.height / 2.2,
-      1700,
-      "#754342"
-    );
-
-    let size = canvas.width / 13;
-    createjs.Tween.get(first)
-      .wait(70 * 9 + 800)
-      .to({ y: canvas.height / 2.2 - size }, 500, createjs.Ease.backInOut);
-
-    text.addChild(first);
-    text.addChild(second);
-
     createjs.Tween.get(background)
       .wait(70 * 12 + 2200)
       .to({ alpha: 0 }, 500);
-
-    createjs.Tween.get(text)
-      .wait(70 * 9 + 800)
-      .to({ scale: 1.1 }, 320, createjs.Ease.cubicOut)
-      .to({ scale: 1 }, 460, createjs.Ease.cubicInOut);
 
     s.addChild(background);
     s.addChild(text);
--- utils/splash_animation.ts
+++ utils/splash_animation.ts
@@ -42,3 +42,42 @@ const buildLetter = (
 
   return element;
 };
+
+export const buildLoading = (canvas: HTMLCanvasElement): createjs.Container => {
+  const text = new createjs.Container();
+  text.scaleX = text.scaleY = 0.6;
+  text.regX = canvas.width / 2;
+  text.regY = canvas.height / 2;
+  text.y = canvas.height / 2;
+
+  const first = buildTextline(
+    canvas.width,
+    "クリエイターなら、",
+    canvas.height / 2.2,
+    500,
+    "#F78E8A"
+  );
+
+  const second = buildTextline(
+    canvas.width,
+    "つくり続けた奴が正義だ。",
+    canvas.height / 2.2,
+    1700,
+    "#754342"
+  );
+
+  let size = canvas.width / 13;
+  createjs.Tween.get(first)
+    .wait(70 * 9 + 800)
+    .to({ y: canvas.height / 2.2 - size }, 500, createjs.Ease.backInOut);
+
+  text.addChild(first);
+  text.addChild(second);
+
+  createjs.Tween.get(text)
+    .wait(70 * 9 + 800)
+    .to({ scale: 1.1 }, 320, createjs.Ease.cubicOut)
+    .to({ scale: 1 }, 460, createjs.Ease.cubicInOut);
+
+  return text;
+};
